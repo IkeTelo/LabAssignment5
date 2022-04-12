1f50a2ce3e0acfb14d1ea598af0c0faf69527c38
Isaac Telloian <iketelloian@fresnostate.edu>
2022-04-12 13:31:42 -0700

    finish code

--- lab_5.py
+++ lab_5.py
@@ -39,3 +39,24 @@ create_dictionary():
 
 main()
 """
+
+def main():
+    sentence = input("Enter a sentence please: ")
+    dictionary = create_dictionary("textese.txt")
+    translate(sentence, dictionary)
+
+
+def create_dictionary(txt_file):
+    infile = open(txt_file, "r")
+    words = [word.rstrip() for word in infile]
+    infile.close()
+    return dict([word.split("," for word in words)])
+
+
+def translate(sentence, dictionary):
+    words = sentence.split()
+    for word in words:
+        print(dictionary.get(word, word), " ", end="")
+
+
+main()
